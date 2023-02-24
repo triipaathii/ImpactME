d3fcb10c3ed4dee495fba107aae6540573eb96db
triipaathii <myself10121990@gmail.com>
2023-02-24 12:43:11 +0530

    firebase added

--- flutter_app/lib/enter_user_number.dart
+++ flutter_app/lib/enter_user_number.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class EnterUserNumber extends StatefulWidget {
   const EnterUserNumber({super.key});
@@ -12,14 +13,23 @@ class _EnterUserNumberState extends State<EnterUserNumber> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Color(0xff243b55),
+    ));
+
     return Container(
       decoration: const BoxDecoration(
           gradient: LinearGradient(
               colors: <Color>[Color(0xff0ccda3), Color(0xff243b55)],
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter)),
-      child: const Scaffold(
-        backgroundColor: Colors.transparent,
+      child: SafeArea(
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Column(
+            children: [Text("Welcome")],
+          ),
+        ),
       ),
     );
   }
--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import './enter_user_number.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
@@ -11,6 +16,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
