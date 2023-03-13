3ec4ffcf87cea87f4cd09f94f28d911121e40493
triipaathii <myself10121990@gmail.com>
2023-03-13 23:26:54 +0530

    new commit

--- flutter_app/lib/models/courses.dart
+++ flutter_app/lib/models/courses.dart
@@ -64,43 +64,35 @@ final courses = <Map<String, dynamic>>[
   },
   {
     'course_id': 1003,
-    'course_name': "Pottery",
-    'description':
-        "    This course offers a thorough, well-organized, and detailed "
-            "approach to the numerous aspects of the of the potter's "
-            "wheel as a mode of artistic expression.  The class moves "
-            "through studio set up and safety, construction processes, "
-            "to artistic aesthetic choices. \n    The class is a thoughtful "
-            "blend of artistic guidance and fundamental educational goals"
-            " for art students, Wheel-Thrown Pottery for Beginners was "
-            "developed for varying levels of art courses and for "
-            "individuals wishing to pursue their own ceramic education. "
-            "The class will enable you to speak knowledgeably about the "
-            "medium, understand techniques, outline clear forming steps,"
-            " and to expand your knowledge of the pottery’s wheel.",
+    'course_name': "Sewing",
+    'description': "This sewing course is suitable for beginners who have little or no"
+        "experience in sewing. The course will cover the basics of using a sewing machine,"
+        "reading patterns, cutting and measuring fabrics, and sewing techniques. By the end of"
+        "the course, students will have the skills to create their own garments and accessories.",
     'course_content': [
-      "Introduction to Clay and the Pottery Wheel",
-      "Throwing Basics",
-      "Cups",
-      "Bowls and Open Forms",
-      "Plates, Flatware and Bats",
-      "Additional Skill Builders",
+      "Introduction to sewing machines and equipment",
+      "Understanding fabric types and their properties",
+      "Measuring and cutting fabrics",
+      "Sewing techniques including hemming, seaming, and inserting zippers",
+      "Reading and understanding patterns",
+      "Making simple garments and accessories",
+      "Finishing techniques such as pressing and ironing"
     ],
     'perks': [
-      "Creative outlet",
-      "Improved focus and concentration",
-      "Increased mindfulness",
-      "Improved hand-eye coordination",
-      "Social Opportunities",
-      "Sense of accomplishment",
-      "Practical skills"
+      "Learn a valuable skill that can be used for personal or professional purposes",
+      "The ability to create your own clothing and accessories",
+      "A fun and creative way to express your personal style",
+      "The potential to save money by creating your own garments and home decor items",
+      "Opportunities to connect with a community of sewing enthusiasts through online",
+      "groups or in-person classes",
+      "Gain a sense of accomplishment and pride in creating something from scratch."
     ],
     'image_path': "assets/images/pottery.png",
     'color': Colors.blueGrey.shade600
   },
   {
     'course_id': 1004,
-    'course_name': "Pottery",
+    'course_name': "Tailoring",
     'description':
         "    This course offers a thorough, well-organized, and detailed "
             "approach to the numerous aspects of the of the potter's "
--- flutter_app/lib/screens/volunteer_registration.dart
+++ flutter_app/lib/screens/volunteer_registration.dart
@@ -1,5 +1,6 @@
 import 'package:country_state_city_pro/country_state_city_pro.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/models/courses.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -26,142 +27,308 @@ class _VolunteerRegistrationState extends State<VolunteerRegistration> {
   final userStateController = TextEditingController();
   final userCountryController = TextEditingController();
   final userPincodeController = TextEditingController();
+  final userQualificationController = TextEditingController();
+  final userOrganizationController = TextEditingController();
+
   List<String> userSkills = [];
   List<String> userLanguages = [];
+
+  List<String> totalSkills = [];
+  List<String> totalLanguages = [];
 
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     return Scaffold(
+        backgroundColor: Colors.white,
         body: SingleChildScrollView(
-      child: Column(
-        children: [
-          SizedBox(
-            height: height * 0.1,
-            width: width,
-          ),
-          Center(
-            child: CircleAvatar(
-              radius: height * 0.1,
-              backgroundColor: Color(0xff243b55),
-              backgroundImage: AssetImage("assets/images/man.png"),
-            ),
-          ),
-          SizedBox(
-            height: height * 0.03,
-          ),
-          Center(
-            child: Text(
-              "SELECT FROM",
-              style: GoogleFonts.raleway(),
-            ),
-          ),
-          SizedBox(
-            height: height * 0.015,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Color(0xff243b55),
-                    borderRadius: BorderRadius.circular(5)),
-                child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.camera_alt_rounded,
-                      color: Colors.white,
-                    )),
+              SizedBox(
+                height: height * 0.1,
+                width: width,
+              ),
+              Center(
+                child: CircleAvatar(
+                  radius: height * 0.1,
+                  backgroundColor: Color(0xff243b55),
+                  backgroundImage: AssetImage("assets/images/man.png"),
+                ),
               ),
               SizedBox(
-                width: width * 0.02,
+                height: height * 0.03,
+              ),
+              Center(
+                child: Text(
+                  "SELECT FROM",
+                  style: GoogleFonts.raleway(),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.015,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xff243b55),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.camera_alt_rounded,
+                          color: Colors.white,
+                        )),
+                  ),
+                  SizedBox(
+                    width: width * 0.02,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xff243b55),
+                        borderRadius: BorderRadius.circular(5)),
+                    child: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.image_rounded,
+                          color: Colors.white,
+                        )),
+                  )
+                ],
+              ),
+              SizedBox(
+                height: height * 0.03,
+              ),
+              Text(
+                "BASIC DETAILS",
+                style: GoogleFonts.raleway(
+                    color: const Color(0xff243b55),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    letterSpacing: 5),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 3,
+                      width: width * 0.175,
+                      color: const Color(0xff243b55),
+                    ),
+                    const Text(
+                      "   X   ",
+                      style: TextStyle(
+                        color: Color(0xff243b55),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      height: 3,
+                      width: width * 0.175,
+                      color: const Color(0xff243b55),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: height * 0.01,
               ),
               Container(
-                decoration: BoxDecoration(
-                    color: Color(0xff243b55),
-                    borderRadius: BorderRadius.circular(5)),
-                child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.image_rounded,
-                      color: Colors.white,
-                    )),
-              )
+                margin: const EdgeInsets.all(10),
+                width: width * 0.8,
+                child: TextFormField(
+                  controller: userEmailAddressController,
+                  style: GoogleFonts.raleway(fontSize: 18),
+                  keyboardType: TextInputType.emailAddress,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      labelText: "Email Address",
+                      labelStyle: GoogleFonts.raleway(fontSize: 17),
+                      floatingLabelStyle:
+                          GoogleFonts.raleway(color: Color(0xff243b55)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 2)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: Color(0xff243b55), width: 2))),
+                ),
+              ),
+              CountryStateCityPicker(
+                country: userCountryController,
+                state: userStateController,
+                city: userCityController,
+                textFieldInputBorder: OutlineInputBorder(
+                    borderSide:
+                        const BorderSide(color: Color(0xff243b55), width: 2)),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+                width: width * 0.8,
+                child: TextFormField(
+                  controller: userPincodeController,
+                  style: GoogleFonts.raleway(fontSize: 18),
+                  keyboardType: TextInputType.number,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      labelText: "Pincode",
+                      labelStyle: GoogleFonts.raleway(fontSize: 17),
+                      floatingLabelStyle:
+                          GoogleFonts.raleway(color: Color(0xff243b55)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 2)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: Color(0xff243b55), width: 2))),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+                width: width * 0.8,
+                child: TextFormField(
+                  controller: userQualificationController,
+                  style: GoogleFonts.raleway(fontSize: 18),
+                  keyboardType: TextInputType.text,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      labelText: "Qualification",
+                      labelStyle: GoogleFonts.raleway(fontSize: 17),
+                      floatingLabelStyle:
+                          GoogleFonts.raleway(color: Color(0xff243b55)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 2)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: Color(0xff243b55), width: 2))),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.all(10),
+                width: width * 0.8,
+                child: TextFormField(
+                  controller: userOrganizationController,
+                  style: GoogleFonts.raleway(fontSize: 18),
+                  keyboardType: TextInputType.text,
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                      labelText: "Organization",
+                      labelStyle: GoogleFonts.raleway(fontSize: 17),
+                      floatingLabelStyle:
+                          GoogleFonts.raleway(color: Color(0xff243b55)),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 2)),
+                      focusedBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                              color: Color(0xff243b55), width: 2))),
+                ),
+              ),
+              SizedBox(
+                height: height * 0.02,
+              ),
+              Text(
+                "SKILLS",
+                style: GoogleFonts.raleway(
+                    color: const Color(0xff243b55),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                    letterSpacing: 5),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      height: 3,
+                      width: width * 0.175,
+                      color: const Color(0xff243b55),
+                    ),
+                    const Text(
+                      "   X   ",
+                      style: TextStyle(
+                        color: Color(0xff243b55),
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Container(
+                      height: 3,
+                      width: width * 0.175,
+                      color: const Color(0xff243b55),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: height * 0.01,
+              ),
+              Text(
+                "Tap on the skills you know or have interest",
+                style: GoogleFonts.raleway(fontWeight: FontWeight.bold),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: width * 0.1),
+                child: GridView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: courses.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10,
+                        mainAxisSpacing: height * 0.01,
+                        childAspectRatio: 2.5),
+                    itemBuilder: (context, index) {
+                      bool selected = false;
+                      return InkWell(
+                        onTap: () {
+                          setState(() {
+                            selected = !selected;
+                          });
+                        },
+                        child: Container(
+                          decoration: BoxDecoration(
+                              color:
+                                  selected ? Color(0xff243b55) : Colors.white,
+                              border: Border.all(
+                                  color: Color(0xff243b55), width: 2),
+                              borderRadius: BorderRadius.circular(20)),
+                          child: Center(
+                            child: Text(
+                              "${courses[index]['course_name']}",
+                              style:
+                                  GoogleFonts.raleway(color: Color(0xff243b55)),
+                            ),
+                          ),
+                        ),
+                      );
+                    }),
+              ),
+
+              SizedBox(
+                height: height * 0.03,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => VolunteerRegistration()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xff243b55)),
+                  child: Text(
+                    "SUBMIT",
+                    style: GoogleFonts.raleway(color: Colors.white),
+                  )),
+              SizedBox(height: height * 0.025),
             ],
           ),
-          SizedBox(
-            height: height * 0.05,
-          ),
-          Container(
-            margin: const EdgeInsets.all(10),
-            width: width * 0.8,
-            child: TextFormField(
-              controller: userEmailAddressController,
-              style: GoogleFonts.raleway(fontSize: 18),
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                  labelText: "Email Address",
-                  labelStyle: GoogleFonts.raleway(fontSize: 17),
-                  floatingLabelStyle:
-                      GoogleFonts.raleway(color: Color(0xff243b55)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          const BorderSide(color: Colors.grey, width: 2)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: Color(0xff243b55), width: 2))),
-            ),
-          ),
-          CountryStateCityPicker(
-            country: userCountryController,
-            state: userStateController,
-            city: userCityController,
-            textFieldInputBorder: OutlineInputBorder(
-                borderSide:
-                    const BorderSide(color: Color(0xff243b55), width: 2)),
-          ),
-          Container(
-            margin: const EdgeInsets.all(10),
-            width: width * 0.8,
-            child: TextFormField(
-              controller: userPincodeController,
-              style: GoogleFonts.raleway(fontSize: 18),
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              decoration: InputDecoration(
-                  labelText: "Pincode",
-                  labelStyle: GoogleFonts.raleway(fontSize: 17),
-                  floatingLabelStyle:
-                      GoogleFonts.raleway(color: Color(0xff243b55)),
-                  enabledBorder: OutlineInputBorder(
-                      borderSide:
-                          const BorderSide(color: Colors.grey, width: 2)),
-                  focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          color: Color(0xff243b55), width: 2))),
-            ),
-          ),
-          SizedBox(
-            height: height * 0.015,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => VolunteerRegistration()));
-              },
-              style:
-                  ElevatedButton.styleFrom(backgroundColor: Color(0xff243b55)),
-              child: Text(
-                "SUBMIT",
-                style: GoogleFonts.raleway(color: Colors.white),
-              )),
-          SizedBox(height: height * 0.025),
-        ],
-      ),
-    ));
+        ));
   }
 }
