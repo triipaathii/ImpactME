8297ead31fe3692b2fdce99761c4f25b1e306338
anushasharma384 <sharmaanusha384@gmail.com>
2023-03-17 14:27:51 +0530

    feed page commit

--- flutter_app/lib/models/courses.dart
+++ flutter_app/lib/models/courses.dart
@@ -360,43 +360,65 @@ final courses = <Map<String, dynamic>>[
 
 final feeds = [
   {
-    'image_path': "assets/images/anurag.jpg",
+    'image_path': "assets/images/feed1.jpg",
     'location': "Kanpur, Uttar Pradesh",
     'description': "The workshop in Kanpur was executed successfully. People enjoyed learning and were eager to learn more. Also, they gave blessings to this wonderful program started by ImpactMe.",
-    'timestamp': "23rd Mar, 2022 | 12:03PM",
+    'timestamp': "23rd Mar, 2022 | 1:00PM",
     'user_name': "Anurag Tripathi",
     'user_image': "assets/images/anurag.jpg",
-    'likes': 21,
-    'comments': 5
+    'likes': 107,
+    'comments': 7
   },
   {
-    'image_path': "assets/images/anurag.jpg",
-    'location': "Kanpur, Uttar Pradesh",
-    'description': "The workshop in Kanpur was executed successfully. People enjoyed learning and were eager to learn more. Also, they gave blessings to this wonderful program started by ImpactMe.",
-    'timestamp': "23rd Mar, 2022 | 12:03PM",
-    'user_name': "Anurag Tripathi",
-    'user_image': "assets/images/anurag.jpg",
-    'likes': 21,
-    'comments': 5
+    'image_path': "assets/images/feed2.jpg",
+    'location': "Mandi, Himachal Pradesh",
+    'description': "I took a workshop at Mandi, Himachal Pradesh. The workshop was about crafting the baskets. Everyone present there was really eager and happy to learn.",
+    'timestamp': "18th Mar, 2022 | 12:55PM",
+    'user_name': "Anusha Sharma",
+    'user_image': "assets/images/anusha.jpeg",
+    'likes': 101,
+    'comments': 9
   },
   {
-    'image_path': "assets/images/anurag.jpg",
-    'location': "Kanpur, Uttar Pradesh",
-    'description': "The workshop in Kanpur was executed successfully. People enjoyed learning and were eager to learn more. Also, they gave blessings to this wonderful program started by ImpactMe.",
-    'timestamp': "23rd Mar, 2022 | 12:03PM",
-    'user_name': "Anurag Tripathi",
-    'user_image': "assets/images/anurag.jpg",
-    'likes': 21,
-    'comments': 5
+    'image_path': "assets/images/feed3.jpg",
+    'location': "Chamba, Himachal Pradesh",
+    'description': "A workshop was conducted by ImpactMe in Chamba, Himachal Pradesh teaching villagers, the skill of pottery. It went really good and they all supported the initiative taken by ImpactMe",
+    'timestamp': "15th Mar, 2022 | 1:03PM",
+    'user_name': "Tanisha Sharma",
+    'user_image': "assets/images/tanisha.jpeg",
+    'likes': 55,
+    'comments': 2
   },
   {
-    'image_path': "assets/images/anurag.jpg",
-    'location': "Kanpur, Uttar Pradesh",
-    'description': "The workshop in Kanpur was executed successfully. People enjoyed learning and were eager to learn more. Also, they gave blessings to this wonderful program started by ImpactMe.",
-    'timestamp': "23rd Mar, 2022 | 12:03PM",
-    'user_name': "Anurag Tripathi",
-    'user_image': "assets/images/anurag.jpg",
-    'likes': 21,
-    'comments': 5
-  }
+    'image_path': "assets/images/feed4.jpg",
+    'location': "Baloh, Hamirpur, Himachal Pradesh",
+    'description': "The workshop in Baloh, hamirpur, Himachal Pradesh went really well. People were eager to learn. Also, they also appreciated this wonderful program started by ImpactMe.",
+    'timestamp': "10th Mar, 2022 | 2:03PM",
+    'user_name': "Adithya Srivastava",
+    'user_image': "assets/images/cops.jpeg",
+    'likes': 51,
+    'comments': 6
+  },
+  {
+    'image_path': "assets/images/feed5.jpg",
+    'location': "Jhaler, Hamirpur, Himachal Pradesh",
+    'description': "I conducted a workshop in Jhaler, Hamirpur, Himachal pradesh. The workshop was all about crafting on the handbags. People out there were intrested in learning and were really supportive.",
+    'timestamp': "23rd Feb, 2022 | 3:33PM",
+    'user_name': "Shreyansh Jain",
+    'user_image': "assets/images/shreyansh.jpg",
+    'likes': 40,
+    'comments': 7
+  },
+  {
+    'image_path': "assets/images/feed6.jpg",
+    'location': "Lambri, Hamirpur, Himachal Pradesh",
+    'description': "I am really happy to tell you all that I conducted a workshop on sewing. I was literally elated to know that people there are really fast learners and teaching them was a lot of fun.",
+    'timestamp': "18th Feb, 2022 | 1:53PM",
+    'user_name': "Sejal Jain",
+    'user_image': "assets/images/sejal.jpeg",
+    'likes': 53,
+    'comments': 1
+  },
 ];
+
+
