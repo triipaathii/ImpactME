fb99ce7f99924bf6c6dcc285b569b02a61cb3b8d
anushasharma384 <sharmaanusha384@gmail.com>
2023-03-26 21:05:42 +0530

    business promotions page

--- flutter_app/lib/models/courses.dart
+++ flutter_app/lib/models/courses.dart
@@ -66,3 +66,140 @@ final feeds = [
     'comments': 1
   },
 ];
+
+final jobs = [
+  {
+    'company_name': "Jaipur Rugs Foundation",
+    'company_image': "assets/images/job1.png",
+    'location': "Loharara, Himachal Pradesh",
+    'job_description':
+        "Our core focus at JRF is to provide skill-building and skill up-gradation training to those artisans who are associated with carpet weaving and thereby substantially increase their income. Since most artisans involved are women, increased income will make meaningful contributions to alleviating poverty. We facilitate income-generating opportunities at the doorsteps of artisans residing in the remotest of the locations and forward & backward linkages with the aid of our strategic partners. Jaipur Rugs is a social enterprise that connects rural craftsmanship with global markets through its luxurious handmade carpets. It is a family run business that offers an exclusive range of hand-knotted and hand-woven rugs made using 2500 years old traditional art form.",
+    'skills': 
+      "Familier to carpet making, "
+      "Carpet and rug handeling, "
+      "Patern Making and "
+      "Dying.",
+    'preferred_gender': "Male/Female",
+    'Preferred_age' : "30-35 yr",
+    'salary': "5000 per month",
+    'timestamp': "23rd Mar, 2022 | 12:03PM",
+    'contact_details': 9015417455
+  },
+  {
+    'company_name': "Loili Industry",
+    'company_image': "assets/images/job2.png",
+    'location': "Morbi, Gujrat",
+    'job_description':
+        "The job role is to do with formulating body compositions and glaze formulations. The candidate should have experience in ceramic tile/sanitary/tableware industry and should have in depth knowledge and understanding of subject.",
+    'skills': 
+      "Machine Handeling, "
+      "Basic knowledge of ceramic material, and "
+      "Practical skills of pottery"
+    ,
+    'preferred_gender': "Male/Female",
+    'Preferred_age' : "30-32 yr",
+    'salary': "10000 per month",
+    'timestamp': "20th Mar, 2022 | 1:03PM",
+    'contact_details': 8954317455
+  },
+  {
+    'company_name': "Unati Co-op",
+    'company_image': "assets/images/job3.png",
+    'location': "Hoshiarpur, Punjab",
+    'job_description':
+        " We are searching for a manager with good business sense to join our team. The farm managers responsibilities include monitoring staff, crops, and livestock, purchasing supplies, preparing budgets and reports, and ensuring maximum profit. You should be able to maintain professional networks and keep abreast of developments in agricultural science.To be successful as a farm manager, you be detail-oriented but also keep the bigger picture in mind.",
+    'skills': 
+      "Candidate should have minimum 5 year experience in managing extension project on organic farming, farmer management, "
+      "Skill in organic certification and documentation as per NOP,NSOP, EU norms."
+    ,
+    'preferred_gender': "Male",
+    'Preferred_age' : "25-35 yr",
+    'salary': "20000 per month",
+    'timestamp': "16th Mar, 2022 | 12:53PM",
+    'contact_details': 9012317455
+  },
+  {
+    'company_name': "Strance Solutions",
+    'company_image': "assets/images/job4.png",
+    'location': "Pikhi, Punjab",
+    'job_description':
+        "Planning for Planting and procurement from farmers of targeted quantity and quality. Advises growers on crop management, advising on problems and alternative solutions, providing technical and planning assistance. Conduct detailed monitoring of the growth and development of crops. Establish Strong relationships with both current and new growers. Conduct a range of field experiments to optimise yields",
+    'skills': 
+      "Candidate should have experience in managing extension project on organic farming, farmer management, and "
+      "Skill in organic farming"
+    ,
+    'preferred_gender': "Male/Female",
+    'Preferred_age' : "25-40 yr",
+    'salary': "6000 per month",
+    'timestamp': "15th Mar, 2022 | 2:09PM",
+    'contact_details': 9085417455
+  },
+  {
+    'company_name': "TekPillar services",
+    'company_image': "assets/images/job5.png",
+    'location': "Chaupal, Himachal Pradesh",
+    'job_description': "Footwear background candidates can apply with quality experience in footwear , who can find defect in shoes and control it",
+    'skills':
+      "Performing manual labor, "
+      "Performing maintenance on the farm, "
+      "Handling heavy machinery and "
+      "Repairing faulty vehicles and machinery."
+    ,
+    'preferred_gender': "Male",
+    'Preferred_age' : "30-35 yr",
+    'salary': "7000 per month",
+    'timestamp': "10th Mar, 2022 | 1:53PM",
+    'contact_details': 8915417455
+  },
+  {
+    'company_name': "f2b Consultancy",
+    'company_image': "assets/images/job6.png",
+    'location': "Roorkee, Uttrakhand",
+    'job_description':
+        "Footwear background candidates can apply with quality experience in footwear , who can find defect in shoes and control it",
+    'skills': 
+      "Introduction to Shoe Designing,  "
+      "Materials and Selection and "
+      "Shoe Design Principles"
+    ,
+    'preferred_gender': "Male/Female",
+    'Preferred_age' : 30-40,
+    'salary': "10000 per month",
+    'timestamp': "2nd Mar, 2022 | 1:43PM",
+    'contact_details': 9046417455
+  },
+  {
+    'company_name': "Adhyapan - The Skills hub",
+    'company_image': "assets/images/job7.png",
+    'location': "New Delhi, Delhi",
+    'job_description':
+        " Responsible for sales of the store. Inventory Management. Maintaining sales reports. Responsible for team management. Responsible for P/L. Shrinkage Control",
+    'skills': 
+      "Materials and Selection, "
+      "Shoe Design Principles, "
+      "Prototyping and Production and "
+      "Branding and Marketing"
+    ,
+    'preferred_gender': "Male",
+    'Preferred_age' : "30-35 yr",
+    'salary': "5000 per month",
+    'timestamp': "10th Feb, 2022 | 01:40PM",
+    'contact_details': 8905417455
+  },
+  {
+    'company_name': "Country Road",
+    'company_image': "assets/images/job8.png",
+    'location': "Kanpur, Uttar Pradesh",
+    'job_description':
+        "Have to live on farm with me and other workers. Age limit- 18-30 Aadhaar card must Desire to get rich in life and single focus to work for 5 years minimum.",
+    'skills': 
+      "Performing manual labor, "
+      "Performing maintenance on the farm."
+    ,
+    'preferred_gender': "Male",
+    'Preferred_age' : "25-40 yr",
+    'salary': "6000 per month",
+    'timestamp': "15th Feb, 2022 | 12:03PM",
+    'contact_details': 9876547455
+  },
+];
